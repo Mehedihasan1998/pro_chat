4f19afbd82910bfe548578759ba493c4e3f369c2
Md. Mehedi Hasan <mehedi5477@gmail.com>
2024-05-11 06:13:02 -0700

    started developing

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          centerTitle: true,
+          leading: Icon(Icons.menu),
+          title: Text("Pro Chat"),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.more_vert_rounded)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:pro_chat/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 2), ()=> Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const HomeScreen())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/icon.png",
+                height: size.height * .4,
+                width: size.width * .5,
+                fit: BoxFit.contain,
+              ),
+              RichText(
+                text: TextSpan(
+                    text: "Created by ",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                    children: const <TextSpan>[
+                      TextSpan(
+                          text: "Md. Mehedi Hasan",
+                          style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 22))
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
